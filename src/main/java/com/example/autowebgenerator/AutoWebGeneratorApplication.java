595c7bf335660c9a265f9f2dc26f6ba597b3363f
lawrence <lawrence900613@gmail.com>
2026-03-02 11:05:00 -0800

    initial set up

---
+++ src/main/java/com/example/autowebgenerator/AutoWebGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.autowebgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoWebGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoWebGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/autowebgenerator/AutoWebGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.autowebgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoWebGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
